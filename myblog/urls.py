aaceabc9b446439822208b7ba0007820efb19a2d
somyadh <som.dhingra@yahoo.in>
2017-10-12 02:19:32 +0530

    added login and logout page

--- myblog/urls.py
+++ myblog/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^blog/',include('blog.urls')),
 	url(r'',include('toader.urls')),
+	url(r'^login/$', auth_views.login, {'template_name': 'toader/login.html'},  name='login'),
+	url(r'^logout/$', auth_views.logout, {'template_name': 'toader/logout.html'}, name='logout'),
 ]
